405aca704c0f4e33d0c8ddee4108991c902d0e14
Antonio Guerriere <antonioguerriere@gmail.com>
2023-09-13 23:05:24 +0100

    added functionality to add items to board

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const buttons = document.querySelectorAll(".board-button")
 
 const Gameboard = (() => {
-  let gameboard = ["x","","","","","","","","",]
+  let gameboard = ["","","","","","","","","",]
   const updateGameboard = () => {
     let i = 0
     buttons.forEach(element => {
@@ -9,6 +9,14 @@ const Gameboard = (() => {
       i++
     });
   }
+  buttons.forEach(element => {
+    element.addEventListener("click", () => {
+      if (!element.innerText) {
+        element.innerText = Gameflow.playerTurn
+        Gameflow.toggle()
+      }
+    })
+  });
   return {updateGameboard}
 })()
 
@@ -19,10 +27,18 @@ const playerFactory = () => {
 
 const Gameflow = (() => {
   let playerTurn = "X"
-  return {playerTurn}
+  const toggle = () => {
+    if(Gameflow.playerTurn === "X") {
+      Gameflow.playerTurn = "O"
+    } else if(Gameflow.playerTurn === "O") {
+      Gameflow.playerTurn = "X"
+    }
+  }
+  return {playerTurn, toggle}
 })()
 
 const PlayerX = playerFactory()
 const PlayerO = playerFactory()
 
 Gameboard.updateGameboard()
+
