f1ac6c5789815bd29dbecc207480e308d33b5930
Antonio Guerriere <antonioguerriere@gmail.com>
2023-09-14 12:02:35 +0100

    working game

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll(".board-button")
+const buttons = document.querySelectorAll('.board-button')
 const one = document.querySelector('.one')
 const two = document.querySelector('.two')
 const three = document.querySelector('.three')
@@ -8,6 +8,8 @@ const six = document.querySelector('.six')
 const seven = document.querySelector('.seven')
 const eight = document.querySelector('.eight')
 const nine = document.querySelector('.nine')
+const scoreboard = document.querySelector('.scoreboard')
+const resetButton = document.querySelector('.reset')
 
 const Gameboard = (() => {
   let gameboard = ["","","","","","","","","",]
@@ -20,25 +22,31 @@ const Gameboard = (() => {
   }
 
   const checkForWinner = () => {
-    if (one.innerText === two.innerText && two.innerText === three.innerText) {
-      console.log(one.innerText)
-    } else if (four.innerText === five.innerText && five.innerText === six.innerText){
-      console.log(four.innerText)
-    } else if (seven.innerText === eight.innerText && eight.innerText === nine.innerText ) {
-      console.log(seven.innerText)
-    } else if (one.innerText === five.innerText && five.innerText === nine.innerText){
-      console.log(one.innerText)
-    } else if (three.innerText === five.innerText && five.innerText === seven.innerText) {
-      console.log(three.innerText)
-    } else if (one.innerText === four.innerText && four.innerText === seven.innerText) {
-      console.log(one.innerText)
-    } else if (two.innerText === five.innerText && five.innerText === eight.innerText) {
-      console.log(two.innerText)
-    } else if ( three.innerText === six.innerText && six.innerText === nine.innerText) {
-      console.log(three.innerText)
+    if (one.innerText.trim() && two.innerText.trim() && three.innerText.trim() && one.innerText === two.innerText && two.innerText === three.innerText) {
+     scoreboard.innerText = `Player ${one.innerText} wins`
+    } else if (four.innerText.trim() && five.innerText.trim() && six.innerText.trim() && four.innerText === five.innerText && five.innerText === six.innerText){
+     scoreboard.innerText = `Player ${four.innerText} wins`
+    } else if (seven.innerText.trim() && eight.innerText.trim() && nine.innerText.trim() && seven.innerText === eight.innerText && eight.innerText === nine.innerText ) {
+     scoreboard.innerText = `Player ${seven.innerText} wins`
+    } else if (one.innerText.trim() && five.innerText.trim() && nine.innerText.trim() && one.innerText === five.innerText && five.innerText === nine.innerText){
+     scoreboard.innerText = `Player ${one.innerText} wins`
+    } else if (three.innerText.trim() && five.innerText.trim() && seven.innerText.trim() && three.innerText === five.innerText && five.innerText === seven.innerText) {
+     scoreboard.innerText = `Player ${three.innerText} wins`
+    } else if (one.innerText.trim() && four.innerText.trim() && seven.innerText.trim() && one.innerText === four.innerText && four.innerText === seven.innerText) {
+     scoreboard.innerText = `Player ${one.innerText} wins`
+    } else if (two.innerText.trim() && five.innerText.trim() && eight.innerText.trim() && two.innerText === five.innerText && five.innerText === eight.innerText) {
+     scoreboard.innerText = `Player ${two.innerText} wins`
+    } else if ( three.innerHTML.trim() && six.innerHTML.trim() && nine.innerHTML.trim()  && three.innerHTML === six.innerHTML  && six.innerHTML  === nine.innerHTML ) {
+     scoreboard.innerHTML = `Player ${three.innerHTML} wins`
     }
   }
-  
+
+  resetButton.addEventListener('click', () => {
+    gameboard = ["","","","","","","","","",]
+    Gameflow.playerTurn = "X"
+    updateGameboard()
+    scoreboard.innerText = "Player X's turn"
+  })
 
   buttons.forEach(element => {
     element.addEventListener("click", () => {
@@ -51,6 +59,8 @@ const Gameboard = (() => {
   });
   return {updateGameboard}
 })()
+
+
 
 const playerFactory = () => {
   let score = 0
@@ -67,8 +77,10 @@ const Gameflow = (() => {
   const toggle = () => {
     if(Gameflow.playerTurn === "X") {
       Gameflow.playerTurn = "O"
+      scoreboard.innerText = "Player 0's turn"
     } else if(Gameflow.playerTurn === "O") {
       Gameflow.playerTurn = "X"
+      scoreboard.innerText = "Player X's turn"
     }
   }
   return {playerTurn, toggle}
