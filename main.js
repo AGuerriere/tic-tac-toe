846d6d23e2c377641b4e60bbfabb17dfafe82272
Antonio Guerriere <antonioguerriere@gmail.com>
2023-09-14 10:01:37 +0100

    added check winner function

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 const buttons = document.querySelectorAll(".board-button")
+const one = document.querySelector('.one')
+const two = document.querySelector('.two')
+const three = document.querySelector('.three')
+const four = document.querySelector('.four')
+const five = document.querySelector('.five')
+const six = document.querySelector('.six')
+const seven = document.querySelector('.seven')
+const eight = document.querySelector('.eight')
+const nine = document.querySelector('.nine')
 
 const Gameboard = (() => {
   let gameboard = ["","","","","","","","","",]
@@ -9,11 +18,34 @@ const Gameboard = (() => {
       i++
     });
   }
+
+  const checkForWinner = () => {
+    if (one.innerText === two.innerText && two.innerText === three.innerText) {
+      console.log(one.innerText)
+    } else if (four.innerText === five.innerText && five.innerText === six.innerText){
+      console.log(four.innerText)
+    } else if (seven.innerText === eight.innerText && eight.innerText === nine.innerText ) {
+      console.log(seven.innerText)
+    } else if (one.innerText === five.innerText && five.innerText === nine.innerText){
+      console.log(one.innerText)
+    } else if (three.innerText === five.innerText && five.innerText === seven.innerText) {
+      console.log(three.innerText)
+    } else if (one.innerText === four.innerText && four.innerText === seven.innerText) {
+      console.log(one.innerText)
+    } else if (two.innerText === five.innerText && five.innerText === eight.innerText) {
+      console.log(two.innerText)
+    } else if ( three.innerText === six.innerText && six.innerText === nine.innerText) {
+      console.log(three.innerText)
+    }
+  }
+  
+
   buttons.forEach(element => {
     element.addEventListener("click", () => {
       if (!element.innerText) {
         element.innerText = Gameflow.playerTurn
         Gameflow.toggle()
+        checkForWinner()
       }
     })
   });
@@ -22,8 +54,13 @@ const Gameboard = (() => {
 
 const playerFactory = () => {
   let score = 0
-  return {score}
+  const wins = () => {
+    score += 1
+  }
+  const getScore = () => score;
+  return {getScore, wins}
 }
+
 
 const Gameflow = (() => {
   let playerTurn = "X"
